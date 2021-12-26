a5aaabc9c98627412e38ca69e46aca08549843c8
BrendanCheong <brendancej82@gmail.com>
2021-12-26 23:56:47 +0800

    started creating new helper functions and classes for API queries

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 
-export default function Home() {
+const Home = () => {
     return (
         <div className={styles.container}>
             <Head>
@@ -66,4 +66,6 @@ export default function Home() {
             </footer>
         </div>
     );
-}
+};
+
+export default Home;
