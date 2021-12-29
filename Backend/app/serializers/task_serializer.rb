6ed51ac1860f522b6f8feaa4102758a269790da5
BrendanCheong <brendancej82@gmail.com>
2021-12-30 01:04:58 +0800

    made tasks api contain more data about its tags

--- Backend/app/serializers/task_serializer.rb
+++ Backend/app/serializers/task_serializer.rb
@@ -1,6 +1,9 @@
 class TaskSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :title, :content, :status, :endDate, :user_id
+  attributes :title, :content, :status, :endDate, :user_id, :created_at, :updated_at
+  attributes :tags do |object|
+    object.tags.as_json
+  end
   belongs_to :user
   has_many :tags
 end
