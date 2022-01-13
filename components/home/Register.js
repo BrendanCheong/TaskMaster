42ff0c2e77cbdc7ed50588d746dd3c0de405f5b4
BrendanCheong <brendancej82@gmail.com>
2022-01-13 15:16:20 +0800

    finised styling login page wipagpage with cool animation

--- components/home/Register.js
+++ components/home/Register.js
@@ -2,71 +2,82 @@ import TextField from "@mui/material/TextField";
 import * as yup from "yup";
 import { useFormik } from "formik";
 
-const Register = () => {
+const Register = ({ flip, setFlipped }) => {
+
+
     return (
-        <div className="z-50 antialiased bg-red-500 font-poppins [perspective: 600px] w-96 md:w-[30rem]">
-            {/* <!-- Content --> */}
-            <div className="w-full pt-[4rem]">
-                <div className="container py-8 mx-auto">
-                    <div className="w-full mx-auto rounded shadow bg-zinc-100">
-                        <div className="w-full px-8 py-4 text-xl text-center text-black border-b border-grey-lighter">Register for a free account</div>
-                        <div className="px-8 py-4">
-                            <div className="flex flex-col mb-4 space-y-5 md:flex-row md:space-x-5 md:space-y-0">
-                                <div className="w-full">
-                                    <TextField
-                                        label="Register Name"
-                                        id="name"
-                                        name="name"
-                                        size="large"
-                                        variant="outlined"
-                                        placeholder="Enter Name"
-                                        fullWidth
-                                        type="text"
-                                        autoComplete="on"
-                                    />
-                                </div>
-                                <div className="w-full">
-                                    <TextField
-                                        label="Register Email"
-                                        id="email"
-                                        name="email"
-                                        size="large"
-                                        variant="outlined"
-                                        placeholder="Enter Email"
-                                        fullWidth
-                                        type="email"
-                                        autoComplete="on"
-                                    />
-                                </div>
-                            </div>
-                            <div className="w-full mb-4">
-                                <TextField
-                                    label="Register Password"
-                                    id="password"
-                                    name="password"
-                                    size="large"
-                                    variant="outlined"
-                                    placeholder="Enter Password"
-                                    fullWidth
-                                    type="password"
-                                    autoComplete="on"
-                                />
-                            </div>
-                            <div className="grid text-center place-items-center">
-                                <button className="inline-flex items-center justify-center w-full px-4 py-2 text-base font-medium text-white transition duration-300 bg-indigo-600 border border-transparent rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 font-roboto" type="submit">
-                                    <svg xmlns="http://www.w3.org/2000/svg" className="w-5 h-5 mr-3 -ml-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M13 9l3 3m0 0l-3 3m3-3H8m13 0a9 9 0 11-18 0 9 9 0 0118 0z" />
-                                    </svg>
-                                    Sign Up
-                                </button>
-                            </div>
-                        </div>
-                        <p className="p-4 my-4 text-center">
-                            <button className="text-sm no-underline transition duration-200 text-grey-dark hover:text-grey-darker hover:text-indigo-600">I already have an account</button>
-                        </p>
+        <div className="container mx-auto py-8 absolute [-webkit-backface-visibility:hidden] [backface-visibility:hidden] w-full rounded bg-zinc-100">
+            <div className="w-full px-8 py-4 text-xl text-center text-black border-b border-grey-lighter">Register for a free account</div>
+            <div className="px-8 py-4">
+                <div className="flex flex-col mb-4 space-y-5 md:flex-row md:space-x-5 md:space-y-0">
+                    <div className="w-full">
+                        <TextField
+                            label="Register Name"
+                            id="name"
+                            name="name"
+                            size="large"
+                            variant="outlined"
+                            placeholder="Enter Name"
+                            fullWidth
+                            type="text"
+                            autoComplete="on"
+                        />
+                    </div>
+                    <div className="w-full">
+                        <TextField
+                            label="Register Email"
+                            id="email"
+                            name="email"
+                            size="large"
+                            variant="outlined"
+                            placeholder="Enter Email"
+                            fullWidth
+                            type="email"
+                            autoComplete="on"
+                        />
                     </div>
                 </div>
+                <div className="w-full mb-4">
+                    <TextField
+                        label="Register Password"
+                        id="password"
+                        name="password"
+                        size="large"
+                        variant="outlined"
+                        placeholder="Enter Password"
+                        fullWidth
+                        type="password"
+                        autoComplete="on"
+                    />
+                </div>
+                <div className="w-full mb-4">
+                    <TextField
+                        label="Confirm Password"
+                        id="confirm_password"
+                        name="password"
+                        size="large"
+                        variant="outlined"
+                        placeholder="Confirm Password"
+                        fullWidth
+                        type="password"
+                        autoComplete="on"
+                    />
+                </div>
+                <div className="grid text-center place-items-center">
+                    <button className="inline-flex items-center justify-center w-full px-4 py-2 text-base font-medium text-white transition duration-300 bg-indigo-600 border border-transparent rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 font-roboto" type="submit">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="w-5 h-5 mr-3 -ml-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M13 9l3 3m0 0l-3 3m3-3H8m13 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+                        </svg>
+                            Sign Up
+                    </button>
+                </div>
             </div>
+            <p className="p-4 my-4 text-center">
+                <button className="text-sm no-underline transition duration-200 text-grey-dark text-slate-800 hover:text-indigo-600"
+                    onClick={() => setFlipped(!flip)}>
+                    I already have an account
+                </button>
+            </p>
         </div>
     );
 };
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Image from "next/image";
 import cool_background from "../public/cool_background.png";
 import Register from "@/home/Register";
@@ -5,18 +6,56 @@ import Login from "@/home/Login";
 
 const Home = () => {
 
+    const [flip, setFlipped] = useState(false);
+    const [disable, setDisable] = useState(false);
+
+    const handleFlip = () => {
+        setFlipped(!flip);
+        if (!disable) {
+            setTimeout(() => {
+                setDisable(true);
+            }, 1000);
+        } else {
+            setDisable(false);
+        }
+    };
+
     return (
-        <div className="fixed flex flex-col items-center justify-center w-screen h-screen overflow-hidden place-content-center place-self-center -z-10" id="register form">
+        <div className="flex flex-col items-center justify-center w-screen h-screen overflow-hidden place-content-center place-self-center -z-10" id="register form">
             <Image
                 alt="background"
                 src={cool_background}
                 layout="fill"
-                objectFit="cover"
+                className="object-cover object-center bg-cover pointer-events-none"
                 quality={100}
                 priority={true}
             />
-            {/* <Register/>
-            <Login/> */}
+            <div className="h-96 [perspective:600px] z-50 w-96 md:w-[30rem]">
+                <div className={"w-full h-full [transform-style:preserve-3d] [transition:_transform_1s] relative" + (
+                    flip
+                        ? " [transform:rotateY(180deg)]"
+                        : ""
+                )} id="flip"
+                >
+                    <div className="absolute w-full h-full text-white font-poppins [-webkit-backface-visibility:hidden] [backface-visibility:hidden] bottom-10" id="front">
+                        {
+                            !disable &&
+                            <>
+                                <Login
+                                    flip={flip}
+                                    setFlipped={handleFlip}
+                                />
+                            </>
+                        }
+                    </div>
+                    <div className="absolute w-full h-full text-white font-poppins [-webkit-backface-visibility:hidden] [backface-visibility:hidden] [transform:rotateY(180deg)] bottom-10" id="back">
+                        <Register
+                            flip={flip}
+                            setFlipped={handleFlip}
+                        />
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
