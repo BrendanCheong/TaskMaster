77a4665c8e091bb6654df87bbd35c0cfc8a6e458
BrendanCheong <brendancej82@gmail.com>
2022-01-15 20:36:39 +0800

    added frontend url to CORS settings

--- Backend/config/initializers/cors.rb
+++ Backend/config/initializers/cors.rb
@@ -2,7 +2,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
         # Allow localhost to access REST API for now, change it to be for specific website only later
         origins 'localhost:8888', '127.0.0.1:8888',
-        /\Ahttp:\/\/192\.168\.0\.\d{1,3}(:\d+)?\z/
+        /\Ahttp:\/\/192\.168\.0\.\d{1,3}(:\d+)?\z/, 'https://task-master-cvwo.vercel.app'
         resource '*', 
             headers: :any, 
             methods: [:get, :post, :patch, :put, :delete, :options, :head],
