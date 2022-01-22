6458e95c16e8a33b47f525e31475ce17e211f810
BrendanCheong <brendancej82@gmail.com>
2022-01-22 12:19:14 +0800

    deployment v2.1

--- components/redux/redux-stores/stores.js
+++ components/redux/redux-stores/stores.js
@@ -1,7 +1,6 @@
 /* eslint-disable no-console */
 import { configureStore } from "@reduxjs/toolkit";
 import thunk from "redux-thunk";
-import { logger } from "../middleware/logger";
 import { refresh } from "../middleware/refresh";
 import taskReducer from "../taskSlice";
 import taskViewReducer from "../taskViewSlice";
